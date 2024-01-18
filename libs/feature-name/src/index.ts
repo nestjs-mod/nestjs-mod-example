e9831c3139620cc721ab4a4b4c1c6b42f7d52ad4
EndyKaufman <admin@site15.ru>
2024-01-18 13:54:16 +0400

    chore: generated on @nestjs-mod/schematics@1.1.2

--- libs/feature-name/src/index.ts
+++ libs/feature-name/src/index.ts
@@ -1,3 +1,3 @@
-export * from './lib/feature-name.environments';
 export * from './lib/feature-name.configuration';
+export * from './lib/feature-name.environments';
 export * from './lib/feature-name.module';
